984f612cb76b165566a5a89dd3bb8b745ed7ea16
Leina33 <dominickrutto@gmail.com>
2020-01-23 10:08:14 +0300

    modify my index.html file with functionalities of my car project -- making car order, choosing color, numbers of seats and engine capacity

--- carapp/urls.py
+++ carapp/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static  
 
 urlpatterns=[
     url(r'^$',views.home_project,name='home_project'),
@@ -7,3 +9,5 @@ urlpatterns=[
     url(r'^search/', views.search_results, name='search_results')
     
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
